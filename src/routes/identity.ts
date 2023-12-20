9c520f239fe16c61885ebedfc80ad1bdae4871e2
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-12-20 10:40:31 +0700

    Use Drizzle for Identity route

---
+++ src/routes/identity.ts
@@ -0,0 +1,45 @@
+import { Static as S, Type as t } from '@sinclair/typebox'
+import { type FastifyPluginAsync } from 'fastify'
+// import { prisma } from 'utils/db'
+import { db } from 'drizzle/db'
+import { IdentityRecordResponseDto } from 'utils/types'
+import { identity } from 'drizzle/schema'
+import { eq } from 'drizzle-orm'
+
+const checkInDto = t.Object({
+	email: t.String({ format: 'email' }),
+})
+
+const routes: FastifyPluginAsync = async (fastify) => {
+	fastify.post<{
+		Body: S<typeof checkInDto>
+		Reply: { 200: S<typeof IdentityRecordResponseDto>; 404: Record<string, never> }
+	}>(
+		'/checkin',
+		{
+			schema: {
+				description: 'Check-in someone through email',
+				tags: ['Identity'],
+				body: checkInDto,
+				response: {
+					200: IdentityRecordResponseDto,
+					404: {}
+				},
+			},
+		},
+		async function (request, reply) {
+			const [person] = await db
+				.update(identity)
+				.set({ checkedIn: true })
+				.where(eq(identity.email, request.body.email))
+				.returning()
+
+			if (!person) {
+				return reply.status(404).send()
+			}
+			return reply.status(200).send(person)
+		},
+	)
+}
+
+export default routes
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,28 +1,63 @@
-import { Type as t } from '@sinclair/typebox';
+import {
+	ArrayOptions,
+	IntersectOptions,
+	NumericOptions,
+	ObjectOptions,
+	SchemaOptions,
+	StringOptions,
+	Type as t,
+	TArray,
+	TInteger,
+	TIntersect,
+	TNumber,
+	TObject,
+	TPartial,
+	TSchema,
+	TString,
+} from '@sinclair/typebox'
+import { createInsertSchema, createSelectSchema } from 'drizzle-typebox'
+import { hacker, identity, judge, sponsor } from '../drizzle/schema'
+
+type TypeboxSchemaOptions<T extends TSchema> = T extends TString
+	? StringOptions
+	: T extends TArray
+	? ArrayOptions
+	: T extends TNumber | TInteger
+	? NumericOptions<number>
+	: T extends TIntersect
+	? IntersectOptions
+	: T extends TObject | TPartial<T>
+	? ObjectOptions
+	: SchemaOptions
+
+export function extendSchema<T extends TSchema>(schema: T, options?: TypeboxSchemaOptions<T>): T {
+	return { ...options, ...schema }
+}
 
 /*
-
-       d8888          888    888      
-      d88888          888    888      
-     d88P888          888    888      
-    d88P 888 888  888 888888 88888b.  
-   d88P  888 888  888 888    888 "88b 
-  d88P   888 888  888 888    888  888 
- d8888888888 Y88b 888 Y88b.  888  888 
-d88P     888  "Y88888  "Y888 888  888 
-          
+8888888     888                   888    d8b 888             
+  888       888                   888    Y8P 888             
+  888       888                   888        888             
+  888   .d88888  .d88b.  88888b.  888888 888 888888 888  888 
+  888  d88" 888 d8P  Y8b 888 "88b 888    888 888    888  888 
+  888  888  888 88888888 888  888 888    888 888    888  888 
+  888  Y88b 888 Y8b.     888  888 Y88b.  888 Y88b.  Y88b 888 
+8888888 "Y88888  "Y8888  888  888  "Y888 888  "Y888  "Y88888 
+                                                         888 
+                                                    Y8b d88P 
+                                                     "Y88P"  
 */
 
-export const AuthRecordBaseObject = t.Object({
-  id: t.String(),
-  email: t.String({ format: 'email', examples: ['test@email.com'] }),
-  password: t.String(),
-  emailVerified: t.Boolean(),
-  role: t.Union([t.Literal('HACKER'), t.Literal('SPONSOR'), t.Literal('JUDGE')]),
-  checkedIn: t.Boolean(),
-});
+export const IdentityRecordSchema = createSelectSchema(identity, {
+	email: (s) => extendSchema(s.email, { format: 'email' }),
+})
 
-export const AuthRecordReplyDto = t.Omit(AuthRecordBaseObject, ['password']);
+export const IdentityRecordResponseDto = t.Omit(IdentityRecordSchema, ['password'])
+
+function WithIdentity(schema: TObject) {
+	// Set additionalProperties "false" to prevent leakage of fields not defined in schema, e.g. "password"
+	return t.Composite([schema, t.Object({ identity: IdentityRecordResponseDto })], { additionalProperties: false })
+}
 
 /*
 
@@ -36,60 +71,55 @@ export const AuthRecordReplyDto = t.Omit(AuthRecordBaseObject, ['password']);
 888    888 "Y888888  "Y8888P 888  888  "Y8888  888     
                                                        
 */
-export const HackerBaseObject = t.Object({
-  id: t.String(),
-  firstName: t.String({ examples: ['First'] }),
-  lastName: t.String({ examples: ['Last'] }),
-  phone: t.String({ examples: ['1112223333'] }),
-  birthDate: t.String({ examples: ['05/05/1999'] }),
-  isMinor: t.Boolean(),
-  school: t.String({ examples: ['Harvard College'] }),
-  country: t.String({ examples: ['USA'] }),
-  major: t.String({ examples: ['Computer Science'] }),
-  gender: t.Union(
-    [
-      t.Literal('Male'),
-      t.Literal('Female'),
-      t.Literal('Nonbinary'),
-      t.Literal('Other'),
-      t.Literal('PreferNot'),
-    ],
-    { examples: ['Other'] },
-  ),
-  ethnicities: t.String({ examples: ['Asian'] }),
-  shirtSize: t.Union(
-    [
-      t.Literal('XSmall'),
-      t.Literal('Small'),
-      t.Literal('Medium'),
-      t.Literal('Large'),
-      t.Literal('XLarge'),
-      t.Literal('XXLarge'),
-    ],
-    { examples: ['XXLarge'] },
-  ),
-  howHeard: t.Array(t.String(), { examples: [['2', '3']], collectionFormat: 'multi' }),
-});
+// export const HackerBaseObject = t.Object({
+// 	id: t.String(),
+// 	firstName: t.String({ examples: ['First'] }),
+// 	lastName: t.String({ examples: ['Last'] }),
+// 	phone: t.String({ examples: ['1112223333'] }),
+// 	birthDate: t.String({ examples: ['05/05/1999'] }),
+// 	isMinor: t.Boolean(),
+// 	school: t.String({ examples: ['Harvard College'] }),
+// 	country: t.String({ examples: ['USA'] }),
+// 	major: t.String({ examples: ['Computer Science'] }),
+// 	gender: t.Union(
+// 		[t.Literal('Male'), t.Literal('Female'), t.Literal('Nonbinary'), t.Literal('Other'), t.Literal('PreferNot')],
+// 		{ examples: ['Other'] },
+// 	),
+// 	ethnicities: t.String({ examples: ['Asian'] }),
+// 	shirtSize: t.Union(
+// 		[
+// 			t.Literal('XSmall'),
+// 			t.Literal('Small'),
+// 			t.Literal('Medium'),
+// 			t.Literal('Large'),
+// 			t.Literal('XLarge'),
+// 			t.Literal('XXLarge'),
+// 		],
+// 		{ examples: ['XXLarge'] },
+// 	),
+// 	howHeard: t.Array(t.String(), { examples: [['2', '3']], collectionFormat: 'multi' }),
+// })
 
-export const HackerResponseDto = t.Composite(
-  [HackerBaseObject, t.Object({ auth: t.Omit(AuthRecordBaseObject, ['password']) })],
-  { additionalProperties: false },
-); // Set additionalProperties "false" to prevent leakage of fields not defined in schema, e.g. "password"
+const HackerSchema = createSelectSchema(hacker)
+
+export const HackerResponseDto = WithIdentity(HackerSchema)
 
 export const HackerCreateDto = t.Composite([
-  t.Omit(HackerBaseObject, ['id', 'isMinor']),
-  t.Object({
-    email: t.String({ format: 'email', examples: ['test@email.com'] }),
-  }), // Add an `email` field in the creation request body
-]);
+	createInsertSchema(hacker),
+	t.Object({
+		email: t.String({ format: 'email', examples: ['test@email.com'] }),
+	}), // Add an `email` field in the creation request body
+])
 
-export const HackerUpdateDto = t.Object({
-  firstName: t.String({ examples: ['First'] }),
-  lastName: t.String({ examples: ['Last'] }),
-  email: t.String({ format: 'email', examples: ['test@email.com'] }),
-  checkedIn: t.Boolean(),
-  emailVerified: t.Boolean(),
-});
+// export const HackerUpdateDto = t.Object({
+// 	firstName: t.String({ examples: ['First'] }),
+// 	lastName: t.String({ examples: ['Last'] }),
+// 	email: t.String({ format: 'email', examples: ['test@email.com'] }),
+// 	checkedIn: t.Boolean(),
+// 	emailVerified: t.Boolean(),
+// })
+
+export const HackerUpdateDto = HackerCreateDto
 
 /*
 
@@ -106,39 +136,29 @@ export const HackerUpdateDto = t.Object({
 888P"                       "Y88P"           
 */
 
-export const JudgeBaseObject = t.Object({
-  id: t.String(),
-  name: t.String(),
-  category: t.String(),
-  company: t.String(),
-});
+export const JudgeBaseObject = createSelectSchema(judge)
 
 export const JudgeCreateDto = t.Composite([
-  t.Omit(JudgeBaseObject, ['id']),
-  t.Object({
-    email: t.String({ format: 'email', examples: ['judge@email.com'] }),
-  }), // Add an `email` field in the creation request body
-]);
+	createInsertSchema(judge),
+	t.Object({
+		email: t.String({ format: 'email', examples: ['judge@email.com'] }),
+	}), // Add an `email` field in the creation request body
+])
 
 export const JudgeUpdateDto = t.Object({
-  category: t.String(),
-  name: t.String(),
-});
+	category: t.String(),
+	name: t.String(),
+})
 
-export const JudgeReplyDto = t.Composite([
-  JudgeBaseObject,
-  t.Object({
-    auth: t.Omit(AuthRecordBaseObject, ['password']),
-  }),
-]);
+export const JudgeReplyDto = WithIdentity(JudgeBaseObject)
 
 export const UploadProjectCsvDto = t.Object({
-  csvFile: t.String({ format: 'binary' }),
-});
+	csvFile: t.String({ format: 'binary' }),
+})
 
 export const UploadProjectCsvReplyDto = t.Object({
-  count: t.Number(),
-});
+	count: t.Number(),
+})
 
 /*
 
@@ -155,22 +175,13 @@ Y88b  d88P 888 d88P Y88..88P 888  888      X88 Y88..88P 888
            888                                                  
 */
 
-export const SponsorBaseObject = t.Object({
-  id: t.String(),
-  name: t.String(),
-  company: t.String(),
-});
+export const SponsorBaseObject = createSelectSchema(sponsor)
 
 export const SponsorCreateDto = t.Composite([
-  t.Omit(SponsorBaseObject, ['id']),
-  t.Object({
-    email: t.String({ format: 'email', examples: ['sponsor@email.com'] }),
-  }), // Add an `email` field in the creation request body
-]);
+	createInsertSchema(sponsor),
+	t.Object({
+		email: t.String({ format: 'email', examples: ['sponsor@email.com'] }),
+	}), // Add an `email` field in the creation request body
+])
 
-export const SponsorReplyDto = t.Composite([
-  SponsorBaseObject,
-  t.Object({
-    auth: t.Omit(AuthRecordBaseObject, ['password']),
-  }),
-]);
+export const SponsorReplyDto = WithIdentity(SponsorBaseObject)
